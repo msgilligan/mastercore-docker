88241e8be8a11edb52520407e2822dad681cd86f
squarestreamio <jro92024@gmail.com>
2014-12-08 16:09:08 -0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -16,7 +16,6 @@ echo -n no > /etc/container_environment/INITRD
 ## Enable Ubuntu Universe and Multiverse.
 sed -i 's/^#\s*\(deb.*universe\)$/\1/g' /etc/apt/sources.list
 sed -i 's/^#\s*\(deb.*multiverse\)$/\1/g' /etc/apt/sources.list
-sed -i 'http://archive.debian.org/debian/ oldstable main' /etc/apt/sources.list
 apt-get update
 
 ## Fix some issues with APT packages.
